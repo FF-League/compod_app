b02b25653bac2ef69d0603080484bf85a3b2dbcb
Felipe Leite Uematsu <felipe.uematsu@digio.com.br>
2021-12-25 17:47:54 -0300

    Redesign on home
    + Updated compod_raised_button.dart
    + Updated home_view.dart
    + Added home_strings.dart
    + Removed .name method from other strings files

--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -1,36 +1,72 @@
-import 'dart:math';
-
 import 'package:compod_app/commons/compod_images.dart';
+import 'package:compod_app/commons/routes.dart';
+import 'package:compod_app/components/widgets/compod_raised_button.dart';
 import 'package:compod_app/home/controller/home_controller.dart';
-import 'package:compod_app/home/view/components/home_tile.dart';
-import 'package:compod_app/home/view/components/home_tile_grid.dart';
+import 'package:compod_app/home/home_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class HomeView extends GetResponsiveView {
-  HomeView({Key? key}) : super(key: key);
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
 
-  static const _buttonList = [
-    HomeTile(type: HomeTileType.doctor, text: 'Solicitar atendimento'),
-    HomeTile(type: HomeTileType.heart, text: 'Seja um voluntário'),
-    HomeTile(type: HomeTileType.anjinho, text: 'Cartilha'),
-    HomeTile(type: HomeTileType.donate, text: 'Doações'),
+  @override
+  get controller => Get.put(HomeController());
+
+  static const buttonPadding = 6.0;
+  static const padding = 16.0;
+
+  static final _buttonList = [
+    CompodRaisedButton(
+      buttonText: HomeStringsEnum.hospitalization.tr,
+      action: () => Get.toNamed(RoutesEnum.hospitalization.route),
+      white: true,
+      padding: buttonPadding,
+    ),
+    CompodRaisedButton(
+      buttonText: HomeStringsEnum.volunteer.tr,
+      action: () {},
+      white: true,
+      padding: buttonPadding,
+    ),
+    CompodRaisedButton(
+      buttonText: HomeStringsEnum.miniBook.tr,
+      action: () => Get.toNamed(RoutesEnum.minibook.route),
+      white: true,
+      padding: buttonPadding,
+    ),
+    CompodRaisedButton(
+      buttonText: HomeStringsEnum.about.tr,
+      action: () {},
+      white: true,
+      padding: buttonPadding,
+    ),
+    CompodRaisedButton(
+      buttonText: HomeStringsEnum.contact.tr,
+      action: () {},
+      white: true,
+      padding: buttonPadding,
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
-    Get.put(HomeController());
-
-    final margin = (context.width - 320) / 3;
-    const padding = 16.0;
-    final height = max((_buttonList.length / 2).ceil() * (margin + 160), (context.height - (6 * padding + 2 * 80)));
-    return Scaffold(
-      body: ListView(
-        children: [
-          Padding(child: Image.asset(CompodImages.whiteLogo, height: 80), padding: const EdgeInsets.all(padding)),
-          SizedBox(height: height, child: HomeTileGrid(margin: margin, buttonList: _buttonList)),
-          Padding(child: Image.asset(CompodImages.osascoLogo, height: 80), padding: const EdgeInsets.all(padding)),
-        ],
+    return Container(
+      color: Get.theme.colorScheme.primary,
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(padding),
+              child: Image.asset(CompodImages.whiteLogo, height: 80),
+            ),
+            Expanded(child: ListView(children: _buttonList)),
+            Padding(
+              child: Image.asset(CompodImages.osascoLogo, height: 80),
+              padding: const EdgeInsets.all(padding),
+            ),
+          ],
+        ),
       ),
     );
   }
